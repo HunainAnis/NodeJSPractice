9b0b147da60e398747aed91f91d87d36db8a4a7a
Muhammad Hunain <53150149+HunainAnis@users.noreply.github.com>
2021-09-15 21:48:21 +0500

    Morgan as logger added!

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/Bootcamps')
-
+const morgan = require('morgan')
 // LOAD ENV VARIABLES
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+//Using Logger
+if(process.env.NODE_ENV==='development') {
+    app.use(morgan('dev'))
+}
 // Mount Routes
 app.use('/api/v1/bootcamps',bootcamps)
 
