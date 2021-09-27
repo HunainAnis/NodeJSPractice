417be4da35d669ac4f5fb98a88b5f1cba358d4ee
Muhammad Hunain <53150149+HunainAnis@users.noreply.github.com>
2021-09-27 12:13:52 +0500

    Connected to Mongoose and Colors are added!

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/Bootcamps')
 const morgan = require('morgan')
+const colors = require('colors')
 const connectDB = require('./config/db')
 
 // LOAD ENV VARIABLES
@@ -21,4 +22,9 @@ app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+ const server = app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+ process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`.red)
+    server.close(()=>process.exit(1))
+ })
